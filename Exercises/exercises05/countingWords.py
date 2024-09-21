4e4b9a67d1b3f09112b519749573496fbc98f899
HexHungryAda <hexhungryada@gmail.com>
2024-09-21 15:53:52 +0200

    Done exercise

---
+++ Exercises/exercises05/countingWords.py
@@ -0,0 +1,5 @@
+sentence = "A picture says more than a thousand words, a matematical formula says more than a thousand pictures."
+words = sentence.split()
+
+print(sentence)
+print(f"There are {len(words)} words in above sentence.")
