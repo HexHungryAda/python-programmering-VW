9512268dc622dcf38223d1a2ab9b5905303ee48d
HexHungryAda <hexhungryada@gmail.com>
2024-09-27 18:16:06 +0200

    read and store test data, changed to numpy array

--- Labs/Labb2/labb2.py
+++ Labs/Labb2/labb2.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
+import numpy as np
 
 filePath = "Data/datapoints.txt"
 dataPoints = []
@@ -16,21 +17,48 @@ with open(filePath, "r") as file:
             try:
                 dataPoints.append((float(numbers[0]), float(numbers[1]), bool(int(numbers[2]))))
             except ValueError:
-                print(f"Error: line {file.lineno} contains incorrect value type. Skipped.")
+                print(f"Error: line contains incorrect value type. Skipped.")
+                # how do error about specific line number? how deal with this stuff in general? 
 
+data_array = np.array(dataPoints)
 
-x = [t[0] for t in dataPoints]
-y = [t[1] for t in dataPoints]
-colors = ['red' if t[2] else 'blue' for t in dataPoints]
+x_values = data_array[:, 0]
+y_values = data_array[:, 1]
+pokemon_type = data_array[:, 2]
 
 fileName = "pokemon_scatter_plot.png"
-plt.scatter(x, y, c=colors)
+plt.title("Pokemon scatter plot")
 plt.xlabel(dimensionLabels[0])
 plt.ylabel(dimensionLabels[1])
-plt.title("Pokemon scatter plot")
+
 red_patch = mpatches.Patch(color="red", label="Pikachu")
 blue_patch = mpatches.Patch(color="blue", label="Pichu")
 plt.legend(handles=[red_patch, blue_patch])
 
+plt.scatter(x_values, y_values, c=pokemon_type, cmap="bwr")
+
 plt.savefig(fileName)
 print(f"{fileName} created")
+
+test_points = []
+filePath = "Data/testpoints.txt"
+
+with open(filePath, "r") as file:
+    file.readline() #skip the first line.
+
+    for line in file:
+        line_parts = line.strip().split('.', 1)
+        data_parts = line_parts[1]
+        numbers = data_parts.strip().replace('(', "").replace(')', "").split(',')
+        if len(numbers) == 2:
+            try:
+               test_points = tuple(map(float, numbers))
+            except ValueError:
+                print(f"Error: line contains incorrect type. Skipped.")
+        print(numbers)
+
+for point in test_points:
+    print(point, end=" ")
+print()
+print(type(test_points[0]))
+print(type(test_points))
