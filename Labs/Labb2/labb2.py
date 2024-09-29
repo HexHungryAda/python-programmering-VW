c1126d1b17ff3e21699b69a22e2139bb8927a655
HexHungryAda <hexhungryada@gmail.com>
2024-09-29 16:20:50 +0200

    Done with finding point shortest distance to a testpoint

--- Labs/Labb2/labb2.py
+++ Labs/Labb2/labb2.py
@@ -55,10 +55,25 @@ with open(filePath, "r") as file:
                test_points = tuple(map(float, numbers))
             except ValueError:
                 print(f"Error: line contains incorrect type. Skipped.")
-        print(numbers)
 
-for point in test_points:
-    print(point, end=" ")
-print()
-print(type(test_points[0]))
-print(type(test_points))
+test_array = np.array(test_points) # didnt work for some reason, probably wrong format, I hate that shit.
+print(test_array)
+
+# compare all the distances to point, and output the argmin index.
+test_point = np.array((0,0))
+shortest_distance = float('inf')
+closest_point_index = None
+
+for i in range(len(data_array)):
+    distance = np.linalg.norm(data_array[i, :2] - test_point)
+    if distance < shortest_distance:
+        shortest_distance = distance
+        closest_point_index = i
+
+closest_point = data_array[closest_point_index]
+
+print(f"Closest point to the testpoint is {closest_point}, distance is about {round(shortest_distance, 2)}")
+if closest_point[2] == 0:
+    print("testpoint classified as Pichu")
+else:
+    print("testpoint classified as Pikachu")
